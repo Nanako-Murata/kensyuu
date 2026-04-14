5a54d01ed67a1d4d21944a88a1b951bffeb5ffd6
mokal <mokal@NM>
2026-04-14 09:45:10 +0900

    2026年4月14日午前10時ごろ

---
+++ ch3/src/ch3/Main3_1.java
@@ -0,0 +1,21 @@
+package ch3;
+/*
+ * 天気による行動の変化をJavaで表す
+ * 分岐
+ */
+public class Main3_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean tenki = true;
+		if (tenki == true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩に行きます");
+		}else {
+			System.out.println("DVDを見ます");
+		}
+		
+
+	}
+
+}
---
+++ ch3/src/ch3/Main3_3.java
@@ -0,0 +1,17 @@
+package ch3;
+
+/*
+ * 波かっこを省略した記述
+ */
+public class Main3_3 {
+	public static void main(String args) {
+		boolean tenki = true;
+		if (tenki == true) {
+			System.out.println("洗濯に行きます");
+			System.out.println("散歩に行きます");
+		} else
+			System.out.println("DVDを見ます");
+
+	}
+
+}
