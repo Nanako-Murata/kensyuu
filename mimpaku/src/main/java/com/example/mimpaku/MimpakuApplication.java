aa5da7d0cc6c08a0ec2c194eb72a2f8d7efb62d2
Nanako-Murata <mokalate.nanako@gmail.com>
2026-05-12 14:17:20 +0900

    2026年5月12日14時コミット

---
+++ mimpaku/src/main/java/com/example/mimpaku/MimpakuApplication.java
@@ -0,0 +1,13 @@
+package com.example.mimpaku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimpakuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MimpakuApplication.class, args);
+	}
+
+}
